e50fbdf7993ccb8c2017ff36533f79816c897ef7
Bantas93 <bantaz.mtdn@gmail.com>
2025-09-16 12:06:17 +0700

    clean command

--- app/lib/auth/setAuthCookies.js
+++ app/lib/auth/setAuthCookies.js
@@ -1,24 +1,23 @@
-"use server"; // supaya pasti jalan di server
+"use server";
 
 import { cookies } from "next/headers";
 
 export async function setAuthCookies(data) {
   const cookieStore = await cookies();
-  // set access token
+
   cookieStore.set("accessToken", data.accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
-    maxAge: 60 * 15, // 15 menit
+    maxAge: 60 * 15,
     path: "/",
   });
 
-  // set refresh token
   cookieStore.set("refreshToken", data.refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: "strict",
-    maxAge: 60 * 60 * 24 * 7, // 7 hari
+    maxAge: 60 * 60 * 24 * 7,
     path: "/",
   });
 }
